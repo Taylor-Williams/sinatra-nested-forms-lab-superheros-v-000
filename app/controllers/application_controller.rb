06a3ce63864fcbec617c5ecedf3b5a6505e75fc1
Taylor Williams <taylor@thewilliams.net>
2018-08-14 22:01:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
       Team.new(params[:team])
       @teams = Team.all
       params[:team][:heroes].each do |hero|
-        Hero.new(hero.map{|k,v| k: v}.join(", "))
+        Hero.new(hero.map{|k,v| "#{k.to_sym} #{v}"}.join(", "))
       end
       @heroes = Hero.all
       erb :team
