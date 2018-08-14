4c01eff4119800d0fc8831e5c391cd311b4def02
Taylor Williams <taylor@thewilliams.net>
2018-08-14 21:46:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require_relative '../models/hero.rb'
 require_relative '../models/team.rb'
+
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
