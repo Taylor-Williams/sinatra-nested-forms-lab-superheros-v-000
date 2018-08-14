d64d899008ce836b675336a0432afab2d9329004
Taylor Williams <taylor@thewilliams.net>
2018-08-14 21:28:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :index
+      erb :super_hero.erb
+    end
+
+    post '/teams' do
+      erb :team.erb
     end
 
 end
